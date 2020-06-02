b5558b418a173efb4095c1ba8cabb94403bb6876
kengooo <64575727+Kengo47@users.noreply.github.com>
2020-06-02 13:57:36 +0900

    #56

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,7 +52,11 @@ class PostsController < ApplicationController
 
   def search
     @search_params = post_search_params
-    @posts = Post.search(@search_params).page(params[:page]).per(6)
+    @posts = Post.search(@search_params).page(params[:page]).per(6).includes(:user, :prefecture, :city)
+  end
+
+  def rank
+    @rank_posts = Post.unscoped.joins(:likes).group(:post_id).order('count(post_id) desc').page(params[:page]).per(6).includes(:user, :prefecture, :city)
   end
 
   private
