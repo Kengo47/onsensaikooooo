b43c787b5ea5fe47079d5663ed3005e797d16adb
kengooo <64575727+Kengo47@users.noreply.github.com>
2020-06-05 23:06:52 +0900

    #67

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'User', type: :system do
+  before do
+    @user = User.create(name: 'TestUser',
+                        email: 'test@example.com',
+                        password: 'password',
+                        confirmed_at: Date.today)
+  end
+
+  it 'ユーザープロフィールを編集する' do
+    visit new_user_session_path
+
+    # ログインする
+    fill_in 'メールアドレス', with: @user.email
+    fill_in 'パスワード', with: @user.password
+
+    click_button 'ログイン'
+    expect(current_path).to eq root_path
+
+    click_link "#{@user.name}さんでログイン中"
+    click_link 'マイページ'
+    expect(current_path).to eq user_path(@user)
+    expect(page).to have_content "#{@user.name}"
+  end
+end
