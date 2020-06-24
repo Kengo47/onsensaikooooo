fd011d6a7780745bfb758dad639845c10a45c62b
kengooo <64575727+Kengo47@users.noreply.github.com>
2020-06-24 21:42:40 +0900

    carrierwaveをcloudfrontを介して配信

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,13 +7,14 @@ CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'onseeeeen-app'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/onseeeeen-app'
-  config.fog_public = false
+  config.fog_directory  = Rails.application.credentials.aws[:fog_directory]
+  config.asset_host = Rails.application.credentials.aws[:asset_host]
+  config.fog_public = true
+  config.fog_attributes = {cache_control: 'max-age=31536000', expires: 1.year.from_now.httpdate}
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: 'ap-northeast-1'
+    region: Rails.application.credentials.aws[:fog_region]
   }
 end
