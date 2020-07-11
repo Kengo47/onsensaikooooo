ca55167e7aa9dfaa5a5e201e10686e148c7c18e3
kengooo <64575727+Kengo47@users.noreply.github.com>
2020-07-11 21:48:40 +0900

    #130 投稿割合グラフの調整

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   def home
     @posts = Post.page(params[:page]).per(12)
     @post_json = Post.all.to_json(only: %i[id name picture latitude longitude])
-    @chart = Prefecture.joins(:posts).group(:name).count
+    @chart = Post.unscope(:order).joins(:prefecture).group('prefectures.name').order('count_all DESC').limit(10).count
   end
 end
